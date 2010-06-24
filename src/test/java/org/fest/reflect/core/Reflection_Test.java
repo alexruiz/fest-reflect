5b55d8b436f64dd85eb7307bf35da34aa79fb4c3
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-06-24 22:15:29 +0000

    Code cleanup.

--- src/test/java/org/fest/reflect/core/Reflection_Test.java
+++ src/test/java/org/fest/reflect/core/Reflection_Test.java
@@ -1,20 +1,20 @@
 /*
  * Created on May 18, 2007
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2007-2009 the original author or authors.
  */
 package org.fest.reflect.core;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -29,49 +29,53 @@ import org.fest.reflect.type.Type;
 
 /**
  * Tests for <code>{@link Reflection}</code>.
- * 
+ *
  * @author Alex Ruiz
  * @author Yvonne Wang
  */
 public class Reflection_Test {
 
   @Test
-  public void should_return_constructor_fluent_interface() {
-    assertTrue(Reflection.constructor() instanceof TargetType);
+  public void should_return_fluent_interface_for_constructor() {
+    verifyTypesAreEqual(TargetType.class, Reflection.constructor());
   }
 
   @Test
-  public void should_return_field_fluent_interface() {
-    assertTrue(Reflection.field("field") instanceof FieldName);
+  public void should_return_fluent_interface_for_field() {
+    verifyTypesAreEqual(FieldName.class, Reflection.field("foo"));
   }
 
   @Test
-  public void should_return_static_field_fluent_interface() {
-    assertTrue(Reflection.staticField("field") instanceof StaticFieldName);
+  public void should_return_fluent_interface_for_static_field() {
+    verifyTypesAreEqual(StaticFieldName.class, Reflection.staticField("foo"));
   }
 
   @Test
-  public void should_return_method_fluent_interface() {
-    assertTrue(Reflection.method("method") instanceof MethodName);
+  public void should_return_fluent_interface_for_method() {
+    verifyTypesAreEqual(MethodName.class, Reflection.method("foo"));
   }
 
   @Test
-  public void should_return_static_method_fluent_interface() {
-    assertTrue(Reflection.staticMethod("method") instanceof StaticMethodName);
+  public void should_return_fluent_interface_for_static_method() {
+    verifyTypesAreEqual(StaticMethodName.class, Reflection.staticMethod("foo"));
   }
 
   @Test
-  public void should_return_type_fluent_interface() {
-    assertTrue(Reflection.type("type") instanceof Type);
+  public void should_return_fluent_interface_for_type() {
+    verifyTypesAreEqual(Type.class, Reflection.type("foo"));
   }
 
   @Test
-  public void should_return_static_inner_class_fluent_interface() {
-    assertTrue(Reflection.staticInnerClass("staticInnerClass") instanceof StaticInnerClassName);
+  public void should_return_fluent_interface_for_static_inner_class() {
+    verifyTypesAreEqual(StaticInnerClassName.class, Reflection.staticInnerClass("foo"));
   }
 
   @Test
-  public void should_return_property_fluent_interface() {
-    assertTrue(Reflection.property("property") instanceof PropertyName);
+  public void should_return_fluent_interface_for_property() {
+    verifyTypesAreEqual(PropertyName.class, Reflection.property("foo"));
+  }
+
+  private void verifyTypesAreEqual(Class<?> expected, Object o) {
+    assertEquals(expected, o.getClass());
   }
 }
