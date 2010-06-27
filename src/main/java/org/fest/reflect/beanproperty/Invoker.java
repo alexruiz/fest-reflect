7f4956db3a689679cb8caeb6fd4919d642c77ea2
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-06-27 04:23:13 +0000

    Code cleanup.

--- src/main/java/org/fest/reflect/beanproperty/Invoker.java
+++ src/main/java/org/fest/reflect/beanproperty/Invoker.java
@@ -1,15 +1,15 @@
 /*
  * Created on Nov 23, 2009
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2009 the original author or authors.
  */
 package org.fest.reflect.beanproperty;
@@ -30,7 +30,7 @@ import org.fest.reflect.reference.TypeRef;
  * Understands the use of instrospection to access a property from a JavaBean.
  * <p>
  * The following is an example of proper usage of this class:
- * 
+ *
  * <pre>
  *   // Retrieves the value of the property "name"
  *   String name = {@link org.fest.reflect.core.Reflection#property(String) property}("name").{@link PropertyName#ofType(Class) ofType}(String.class).{@link PropertyType#in(Object) in}(person).{@link Invoker#get() get}();
@@ -45,11 +45,11 @@ import org.fest.reflect.reference.TypeRef;
  *   {@link org.fest.reflect.core.Reflection#staticField(String) property}("count").{@link StaticFieldName#ofType(Class) ofType}(int.class).{@link StaticFieldType#in(Class) in}(Person.class).{@link Invoker#set(Object) set}(3);
  * </pre>
  * </p>
- * 
+ *
  * @param <T> the declared type for the property to access.
- * 
+ *
  * @author Alex Ruiz
- * 
+ *
  * @since 1.2
  */
 public final class Invoker<T> {
@@ -68,6 +68,7 @@ public final class Invoker<T> {
     return new Invoker<T>(name, target, descriptor);
   }
 
+  // TODO add Javadoc
   public static PropertyDescriptor descriptorForProperty(String propertyName, Object target) {
     BeanInfo beanInfo = null;
     Class<?> type = target.getClass();
