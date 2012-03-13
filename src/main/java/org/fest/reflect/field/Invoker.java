0ade46fab5babd44129fc958c3b87cfc0d9c847a
Ivan Hristov <hristov.iv@gmail.com>
2012-03-13 21:29:24 +0100

    Fixing formatting

--- src/main/java/org/fest/reflect/field/Invoker.java
+++ src/main/java/org/fest/reflect/field/Invoker.java
@@ -1,15 +1,15 @@
 /*
  * Created on Oct 31, 2006
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
+ * 
  * Copyright @2006-2009 the original author or authors.
  */
 package org.fest.reflect.field;
@@ -49,116 +49,107 @@ import org.fest.reflect.reference.TypeRef;
  * 
  * </p>
  * 
- * @param <T>
- *            the declared type for the field to access.
+ * @param <T> the declared type for the field to access.
  * 
  * @author Alex Ruiz
  * @author Ivan Hristov
  */
 public final class Invoker<T> {
 
-	private final Object target;
-	private final Field field;
-	private final boolean accessible;
-	private final Class<?> expectedType;
+  private final Object target;
+  private final Field field;
+  private final boolean accessible;
+  private final Class<?> expectedType;
 
-	static <T> Invoker<T> newInvoker(String fieldName, TypeRef<T> expectedType, Object target) {
-		return createInvoker(fieldName, expectedType.rawType(), target);
-	}
+  static <T> Invoker<T> newInvoker(String fieldName, TypeRef<T> expectedType, Object target) {
+    return createInvoker(fieldName, expectedType.rawType(), target);
+  }
 
-	static <T> Invoker<T> newInvoker(String fieldName, Class<T> expectedType, Object target) {
-		return createInvoker(fieldName, expectedType, target);
-	}
+  static <T> Invoker<T> newInvoker(String fieldName, Class<T> expectedType, Object target) {
+    return createInvoker(fieldName, expectedType, target);
+  }
 
-	private static <T> Invoker<T> createInvoker(String fieldName, Class<?> expectedType, Object target) {
-		if (target == null) {
-			throw new NullPointerException("Target should not be null");
-		}
-		Field field = lookupInClassHierarchy(fieldName, typeOf(target));
-		verifyCorrectType(field, expectedType);
-		return new Invoker<T>(target, field, expectedType);
-	}
+  private static <T> Invoker<T> createInvoker(String fieldName, Class<?> expectedType, Object target) {
+    if (target == null) { throw new NullPointerException("Target should not be null"); }
+    Field field = lookupInClassHierarchy(fieldName, typeOf(target));
+    verifyCorrectType(field, expectedType);
+    return new Invoker<T>(target, field, expectedType);
+  }
 
-	private static Class<?> typeOf(Object target) {
-		if (target instanceof Class<?>)
-			return (Class<?>) target;
-		return target.getClass();
-	}
+  private static Class<?> typeOf(Object target) {
+    if (target instanceof Class<?>) return (Class<?>) target;
+    return target.getClass();
+  }
 
-	private static Field lookupInClassHierarchy(String fieldName, Class<?> declaringType) {
-		Field field = null;
-		Class<?> target = declaringType;
-		while (target != null) {
-			field = field(fieldName, target);
-			if (field != null)
-				break;
-			target = target.getSuperclass();
-		}
-		if (field != null)
-			return field;
-		throw new ReflectionError(concat("Unable to find field ", quote(fieldName), " in ", declaringType.getName()));
-	}
+  private static Field lookupInClassHierarchy(String fieldName, Class<?> declaringType) {
+    Field field = null;
+    Class<?> target = declaringType;
+    while (target != null) {
+      field = field(fieldName, target);
+      if (field != null) break;
+      target = target.getSuperclass();
+    }
+    if (field != null) return field;
+    throw new ReflectionError(concat("Unable to find field ", quote(fieldName), " in ", declaringType.getName()));
+  }
 
-	private static void verifyCorrectType(Field field, Class<?> expectedType) {
-		boolean isAccessible = field.isAccessible();
-		try {
-			makeAccessible(field);
-			Class<?> actualType = field.getType();
-			if (!expectedType.isAssignableFrom(actualType))
-				throw incorrectFieldType(field, actualType, expectedType);
-		} finally {
-			setAccessibleIgnoringExceptions(field, isAccessible);
-		}
-	}
+  private static void verifyCorrectType(Field field, Class<?> expectedType) {
+    boolean isAccessible = field.isAccessible();
+    try {
+      makeAccessible(field);
+      Class<?> actualType = field.getType();
+      if (!expectedType.isAssignableFrom(actualType)) throw incorrectFieldType(field, actualType, expectedType);
+    } finally {
+      setAccessibleIgnoringExceptions(field, isAccessible);
+    }
+  }
 
-	private Invoker(Object target, Field field, Class<?> expectedType) {
-		this.target = target;
-		this.field = field;
-		this.expectedType = expectedType;
-		accessible = field.isAccessible();
-	}
+  private Invoker(Object target, Field field, Class<?> expectedType) {
+    this.target = target;
+    this.field = field;
+    this.expectedType = expectedType;
+    accessible = field.isAccessible();
+  }
 
-	private static Field field(String fieldName, Class<?> declaringType) {
-		try {
-			return declaringType.getDeclaredField(fieldName);
-		} catch (NoSuchFieldException e) {
-			return null;
-		}
-	}
+  private static Field field(String fieldName, Class<?> declaringType) {
+    try {
+      return declaringType.getDeclaredField(fieldName);
+    } catch (NoSuchFieldException e) {
+      return null;
+    }
+  }
 
-	private static ReflectionError incorrectFieldType(Field field, Class<?> actual, Class<?> expected) {
-		String fieldTypeName = field.getDeclaringClass().getName();
-		String message = concat("The type of the field ", quote(field.getName()), " in ", fieldTypeName, " should be <", expected.getName(), "> but was <", actual.getName(), ">");
-		throw new ReflectionError(message);
-	}
+  private static ReflectionError incorrectFieldType(Field field, Class<?> actual, Class<?> expected) {
+    String fieldTypeName = field.getDeclaringClass().getName();
+    String message = concat("The type of the field ", quote(field.getName()), " in ", fieldTypeName, " should be <",
+        expected.getName(), "> but was <", actual.getName(), ">");
+    throw new ReflectionError(message);
+  }
 
-	/**
-	 * Sets a value in the field managed by this class.
-	 * 
-	 * @param value
-	 *            the value to set.
-	 * @throws ReflectionError
-	 *             if the given value cannot be set.
-	 */
-	public void set(T value) {
-		try {
-			setAccessible(field, true);
-			field.set(target, value);
-		} catch (Exception e) {
-			throw new ReflectionError(concat("Unable to update the value in field ", quote(field.getName())), e);
-		} finally {
-			setAccessibleIgnoringExceptions(field, accessible);
-		}
-	}
+  /**
+   * Sets a value in the field managed by this class.
+   * 
+   * @param value the value to set.
+   * @throws ReflectionError if the given value cannot be set.
+   */
+  public void set(T value) {
+    try {
+      setAccessible(field, true);
+      field.set(target, value);
+    } catch (Exception e) {
+      throw new ReflectionError(concat("Unable to update the value in field ", quote(field.getName())), e);
+    } finally {
+      setAccessibleIgnoringExceptions(field, accessible);
+    }
+  }
 
-	/**
-	 * Decorates a targeted object's methods; Each execution of a targeted
-	 * object's method will be first performed on the same method of the
-	 * {@code decorator} object. The result (if any) from the invocation of the
-	 * targeted object's method will be returned. Note that the type of a
-	 * targeted object should be an interface for this functionality to work.
-	 * 
-	 * <pre>
+  /**
+   * Decorates a targeted object's methods; Each execution of a targeted object's method will be first performed on the
+   * same method of the {@code decorator} object. The result (if any) from the invocation of the targeted object's
+   * method will be returned. Note that the type of a targeted object should be an interface for this functionality to
+   * work.
+   * 
+   * <pre>
 	 * Example: 
 	 * 	I.) Assuming we have the following scenario:
 	 * 
@@ -189,31 +180,26 @@ public final class Invoker<T> {
 	 *	field("uploadFileService").ofType(IUploadFileService.class).in(fileManager).addBefore(uploadFileServiceDecorator); 
 	 * 
 	 * </pre>
-	 * 
-	 * @param decorator
-	 *            which methods be called before the same targeted object
-	 *            methods
-	 */
-	public Invoker<T> addBefore(T decorator) {
-		InvocationHandler handler = new BeforeHandler<T>(get(), decorator);
+   * 
+   * @param decorator which methods be called before the same targeted object methods
+   */
+  public Invoker<T> addBefore(T decorator) {
+    InvocationHandler handler = new BeforeHandler<T>(get(), decorator);
 
-		@SuppressWarnings("unchecked")
-		T f = (T) Proxy.newProxyInstance(decorator.getClass().getClassLoader(),//
-				new Class[] { expectedType }, handler);
+    @SuppressWarnings("unchecked") T f = (T) Proxy.newProxyInstance(decorator.getClass().getClassLoader(),//
+        new Class[] { expectedType }, handler);
 
-		set(f);
+    set(f);
 
-		return this;
-	}
+    return this;
+  }
 
-	/**
-	 * Decorates a targeted object's methods; After each execution of a
-	 * targeted object's method the same method will be executed on the
-	 * {@code decorator} object. The result (if any) from the invocation of the
-	 * targeted object's method will be returned. Note that the type of a
-	 * targeted object should be an interface for this functionality to work.
-	 * 
-	 * <pre>
+  /**
+   * Decorates a targeted object's methods; After each execution of a targeted object's method the same method will be
+   * executed on the {@code decorator} object. The result (if any) from the invocation of the targeted object's method
+   * will be returned. Note that the type of a targeted object should be an interface for this functionality to work.
+   * 
+   * <pre>
 	 * Example: 
 	 * 	I.) Assuming we have the following scenario:
 	 * 
@@ -244,86 +230,81 @@ public final class Invoker<T> {
 	 *	field("uploadFileService").ofType(IUploadFileService.class).in(fileManager).addAfter(uploadFileServiceDecorator); 
 	 * 
 	 * </pre>
-	 * 
-	 * @param decorator
-	 *            which methods be called after the same targeted object
-	 *            methods
-	 */
-	public Invoker<T> addAfter(T decorator) {
-		InvocationHandler handler = new AfterHandler<T>(get(), decorator);
+   * 
+   * @param decorator which methods be called after the same targeted object methods
+   */
+  public Invoker<T> addAfter(T decorator) {
+    InvocationHandler handler = new AfterHandler<T>(get(), decorator);
 
-		@SuppressWarnings("unchecked")
-		T f = (T) Proxy.newProxyInstance(decorator.getClass().getClassLoader(),//
-				new Class[] { expectedType }, handler);
+    @SuppressWarnings("unchecked") T f = (T) Proxy.newProxyInstance(decorator.getClass().getClassLoader(),//
+        new Class[] { expectedType }, handler);
 
-		set(f);
+    set(f);
 
-		return this;
-	}
+    return this;
+  }
 
-	private static final class BeforeHandler<T> implements InvocationHandler {
+  private static final class BeforeHandler<T> implements InvocationHandler {
 
-		private final T target;
-		private final T decorator;
+    private final T target;
+    private final T decorator;
 
-		public BeforeHandler(T target, T decorator) {
-			this.target = target;
-			this.decorator = decorator;
-		}
+    public BeforeHandler(T target, T decorator) {
+      this.target = target;
+      this.decorator = decorator;
+    }
 
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-			method.invoke(decorator, args);
+      method.invoke(decorator, args);
 
-			return method.invoke(target, args);
-		}
-	}
+      return method.invoke(target, args);
+    }
+  }
 
-	private static final class AfterHandler<T> implements InvocationHandler {
+  private static final class AfterHandler<T> implements InvocationHandler {
 
-		private final T target;
-		private final T decorator;
+    private final T target;
+    private final T decorator;
 
-		public AfterHandler(T target, T decorator) {
-			this.target = target;
-			this.decorator = decorator;
-		}
+    public AfterHandler(T target, T decorator) {
+      this.target = target;
+      this.decorator = decorator;
+    }
 
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-			Object result = method.invoke(target, args);
+      Object result = method.invoke(target, args);
 
-			method.invoke(decorator, args);
+      method.invoke(decorator, args);
 
-			return result;
-		}
-	}
+      return result;
+    }
+  }
 
-	/**
-	 * Returns the value of the field managed by this class.
-	 * 
-	 * @return the value of the field managed by this class.
-	 * @throws ReflectionError
-	 *             if the value of the field cannot be retrieved.
-	 */
-	@SuppressWarnings("unchecked")
-	public T get() {
-		try {
-			setAccessible(field, true);
-			return (T) field.get(target);
-		} catch (Exception e) {
-			throw new ReflectionError(concat("Unable to obtain the value in field " + quote(field.getName())), e);
-		} finally {
-			setAccessibleIgnoringExceptions(field, accessible);
-		}
-	}
+  /**
+   * Returns the value of the field managed by this class.
+   * 
+   * @return the value of the field managed by this class.
+   * @throws ReflectionError if the value of the field cannot be retrieved.
+   */
+  @SuppressWarnings("unchecked") public T get() {
+    try {
+      setAccessible(field, true);
+      return (T) field.get(target);
+    } catch (Exception e) {
+      throw new ReflectionError(concat("Unable to obtain the value in field " + quote(field.getName())), e);
+    } finally {
+      setAccessibleIgnoringExceptions(field, accessible);
+    }
+  }
 
-	/**
-	 * Returns the "real" field managed by this class.
-	 * 
-	 * @return the "real" field managed by this class.
-	 */
-	public Field info() {
-		return field;
-	}
+  /**
+   * Returns the "real" field managed by this class.
+   * 
+   * @return the "real" field managed by this class.
+   */
+  public Field info() {
+    return field;
+  }
 }
--- src/test/java/org/fest/reflect/field/FieldDecoratorTest.java
+++ src/test/java/org/fest/reflect/field/FieldDecoratorTest.java
@@ -19,112 +19,110 @@ import org.junit.Test;
  */
 public class FieldDecoratorTest {
 
-	private static class LogMyName {
+  private static class LogMyName {
 
-		private final ILogger logger = new MySmartLogger(this.getClass());
-		private final String name;
+    private final ILogger logger = new MySmartLogger(this.getClass());
+    private final String name;
 
-		public LogMyName(String name) {
-			this.name = name;
-		}
+    public LogMyName(String name) {
+      this.name = name;
+    }
 
-		public void logMyName() {
-			logger.info(name);
-		}
-	}
+    public void logMyName() {
+      logger.info(name);
+    }
+  }
 
-	private static class MySmartLogger implements ILogger {
-		private final Class<?> _clazz;
+  private static class MySmartLogger implements ILogger {
+    private final Class<?> _clazz;
 
-		public MySmartLogger(Class<?> _clazz) {
-			this._clazz = _clazz;
-		}
+    public MySmartLogger(Class<?> _clazz) {
+      this._clazz = _clazz;
+    }
 
-		public void info(String infoStr) {
-			System.out.println(_clazz.getSimpleName() + " : " + infoStr);
-		}
-	}
+    public void info(String infoStr) {
+      System.out.println(_clazz.getSimpleName() + " : " + infoStr);
+    }
+  }
 
-	public interface ILogger {
-		public void info(String infoStr);
-	}
+  public interface ILogger {
+    public void info(String infoStr);
+  }
 
-	@Test
-	public void shouldAddDecoratorBeforeLoggerField() {
-		// GIVEN
-		ILogger mySmartLoggerMock = mock(ILogger.class);
+  @Test public void shouldAddDecoratorBeforeLoggerField() {
+    // GIVEN
+    ILogger mySmartLoggerMock = mock(ILogger.class);
 
-		LogMyName logMyName = new LogMyName("FEST");
+    LogMyName logMyName = new LogMyName("FEST");
 
-		field("logger").ofType(ILogger.class).in(logMyName).addBefore(mySmartLoggerMock);
+    field("logger").ofType(ILogger.class).in(logMyName).addBefore(mySmartLoggerMock);
 
-		// WHEN
-		logMyName.logMyName();
+    // WHEN
+    logMyName.logMyName();
 
-		// THEN
-		verify(mySmartLoggerMock, times(1)).info(eq("FEST"));
-	}
+    // THEN
+    verify(mySmartLoggerMock, times(1)).info(eq("FEST"));
+  }
 
-	private interface IUploadFileService {
-		boolean upload(String file, String destination);
-	}
+  private interface IUploadFileService {
+    boolean upload(String file, String destination);
+  }
 
-	private class FileManager {
+  private class FileManager {
 
-		private static final String DEFAULT_DESTINATION = "http://example.org/default/destination/";
-		
-		private final IUploadFileService uploadFileService;
+    private static final String DEFAULT_DESTINATION = "http://example.org/default/destination/";
 
-		public FileManager() {
-			this.uploadFileService = new IUploadFileService() {
-				
-				public boolean upload(String file, String destination) {
-					return false;
-				}
-			};
-		}
-		
-		public void manage(String fileName) {
-			if (uploadFileService.upload(fileName, DEFAULT_DESTINATION)) {
-				System.out.println("File " + fileName + " sent to " + DEFAULT_DESTINATION);
-			} else {
-				System.out.println("Unable to sent " + fileName + " to " + DEFAULT_DESTINATION);
-			}
-		}
-	}
-	
-	@Test
-	public void shouldAddDecoratorAfterUploadFileService() {
-		// GIVEN
-		IUploadFileService uploadFileServiceMock = mock(IUploadFileService.class);
-		
-		FileManager fileManager = new FileManager();
+    private final IUploadFileService uploadFileService;
 
-		field("uploadFileService").ofType(IUploadFileService.class).in(fileManager).addAfter(uploadFileServiceMock);
+    public FileManager() {
+      this.uploadFileService = new IUploadFileService() {
 
-		// WHEN
-		String fileName = "testFileName";
-		fileManager.manage(fileName);
+        public boolean upload(String file, String destination) {
+          return false;
+        }
+      };
+    }
 
-		// THEN
-		verify(uploadFileServiceMock, times(1)).upload(eq("testFileName"), anyString());
-	}
+    public void manage(String fileName) {
+      if (uploadFileService.upload(fileName, DEFAULT_DESTINATION)) {
+        System.out.println("File " + fileName + " sent to " + DEFAULT_DESTINATION);
+      } else {
+        System.out.println("Unable to sent " + fileName + " to " + DEFAULT_DESTINATION);
+      }
+    }
+  }
 
-	@Test
-	public void shouldAddDecoratorBeforeAndAfterUploadFileService() {
-		// GIVEN
-		IUploadFileService uploadFileServiceMock = mock(IUploadFileService.class);
-		
-		FileManager fileManager = new FileManager();
+  @Test public void shouldAddDecoratorAfterUploadFileService() {
+    // GIVEN
+    IUploadFileService uploadFileServiceMock = mock(IUploadFileService.class);
 
-		field("uploadFileService").ofType(IUploadFileService.class).in(fileManager).addBefore(uploadFileServiceMock).addAfter(uploadFileServiceMock);
+    FileManager fileManager = new FileManager();
 
-		// WHEN
-		String fileName = "testFileName";
-		fileManager.manage(fileName);
+    field("uploadFileService").ofType(IUploadFileService.class).in(fileManager).addAfter(uploadFileServiceMock);
 
-		// THEN
-		verify(uploadFileServiceMock, times(2)).upload(eq("testFileName"), anyString());
-	}
+    // WHEN
+    String fileName = "testFileName";
+    fileManager.manage(fileName);
+
+    // THEN
+    verify(uploadFileServiceMock, times(1)).upload(eq("testFileName"), anyString());
+  }
+
+  @Test public void shouldAddDecoratorBeforeAndAfterUploadFileService() {
+    // GIVEN
+    IUploadFileService uploadFileServiceMock = mock(IUploadFileService.class);
+
+    FileManager fileManager = new FileManager();
+
+    field("uploadFileService").ofType(IUploadFileService.class).in(fileManager).addBefore(uploadFileServiceMock)
+        .addAfter(uploadFileServiceMock);
+
+    // WHEN
+    String fileName = "testFileName";
+    fileManager.manage(fileName);
+
+    // THEN
+    verify(uploadFileServiceMock, times(2)).upload(eq("testFileName"), anyString());
+  }
 
 }
